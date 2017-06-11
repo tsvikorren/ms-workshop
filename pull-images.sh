cb7c03d2e08b6da646da080ef120ef64d238f2b9
tsvikorren <tsvikorren@gmail.com>
2017-06-10 20:14:24 -0400

    Create pull-images.sh

--- pull-images.sh
+++ pull-images.sh
@@ -1,3 +1,4 @@
+docker login containercamp.azurecr.io -u containercamp -p 9=zf+uWPdgEbIdyC1LJaagAafw/W6Uxf
 docker pull containercamp.azurecr.io/aquasec/server:2.1.5
 docker pull containercamp.azurecr.io/aquasec/gateway:2.1.5
 docker pull containercamp.azurecr.io/aquasec/agent:2.1.5
