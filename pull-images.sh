1ebbca07a720cf965e8654791111e1173263f460
tsvikorren <tsvikorren@gmail.com>
2017-06-10 20:10:09 -0400

    Create pull-images.sh

---
+++ pull-images.sh
@@ -0,0 +1,5 @@
+docker pull containercamp.azurecr.io/aquasec/server:2.1.5
+docker pull containercamp.azurecr.io/aquasec/gateway:2.1.5
+docker pull containercamp.azurecr.io/aquasec/agent:2.1.5
+docker pull containercamp.azurecr.io/aquasec/scanner-cli:2.1.5
+docker pull postgres:9.6.1-alpine
